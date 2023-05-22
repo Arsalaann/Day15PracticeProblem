b451c9d2a26800e838678da0ce711f935d92ea0e
Arsalaann <arsalankhanxk@gmail.com>
2023-05-23 01:03:04 +0530

    Hashtable frequency big para

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace Day15Solutions{
     class Program{
-        public static void main(string [] args){
-            Console.WriteLine("Enter a sentence: ");
-            string[] Sentence=Console.ReadLine().Split(' ');
+        public static void Main(string [] args){
+            //Console.WriteLine("Enter a sentence: ");
+            string temp="Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] Sentence=temp.Split(' ');
             MyHashTable HashTable=new MyHashTable();
             foreach(string tmp in Sentence)
                 HashTable.Add(tmp);
