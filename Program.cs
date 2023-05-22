546824ef8af2e6f1822cd07a890d6935447d956c
Arsalaann <arsalankhanxk@gmail.com>
2023-05-23 01:15:27 +0530

    BST add more node

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@
             BinarySearchTree Tree=new BinarySearchTree(56);
             Tree.Add(30);
             Tree.Add(70);
+            Tree.Add(22);
+            Tree.Add(40);
+            Tree.Add(60);
+            Tree.Add(95);
+            Tree.Add(30);
+            Tree.Add(11);
+            Tree.Add(65);
+            Tree.Add(3);
+            Tree.Add(16);
+            Tree.Add(63);
+            Tree.Add(67);
         }
     }
 }
